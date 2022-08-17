e277868f63e67fd7ffe36457238ea7a375acd95c
dmitriy-ga <Dmitriy2243@yandex.ru>
2022-08-17 14:05:30 +0500

    add gendiff_generator

---
+++ gendiff/gendiff_generator.py
@@ -0,0 +1,35 @@
+from collections import OrderedDict
+import json
+
+
+def generate_diff(file1, file2):
+    # Loading files to dictionaries
+    dict1 = json.load(open(file1))
+    dict2 = json.load(open(file2))
+
+    # Dictionary of untouched items
+    stays = dict(dict1.items() & dict2.items())
+
+    # Dictionary of deleted items
+    deletes_wo_syms = dict(dict1.items() - dict2.items())
+    deletes = {}
+    for i in deletes_wo_syms.keys():
+        new_name = '- ' + i
+        deletes[new_name] = deletes_wo_syms[i]
+    del deletes_wo_syms
+    print(deletes)
+
+    # Dictionary of added items
+    adds_wo_syms = dict(dict2.items() - dict1.items())
+    adds = {}
+    for i in adds_wo_syms.keys():
+        new_name = '+ ' + i
+        adds[new_name] = adds_wo_syms[i]
+    del adds_wo_syms
+    print(adds)
+
+    # Python dictionary with diffs
+    diffs = OrderedDict(stays | deletes | adds)
+    for name in sorted(diffs, key=lambda x: x.split()[-1]):
+        diffs.move_to_end(name)
+    print(diffs)
