35416c6190013c4ea69951e8d9ffb36efbd6f50f
dmitriy-ga <Dmitriy2243@yandex.ru>
2022-08-29 16:29:46 +0500

    added tests for nested dictionaries

---
+++ gendiff/tests/test_gendiff_nested.py
@@ -0,0 +1,16 @@
+from gendiff.gendiff_generator import generate_diff
+
+
+def test_flat_example():
+    file1_json = 'gendiff/tests/fixtures/nested/file1.json'
+    file2_json = 'gendiff/tests/fixtures/nested/file2.json'
+    file1_yaml = 'gendiff/tests/fixtures/nested/file1.yaml'
+    file2_yaml = 'gendiff/tests/fixtures/nested/file2.yaml'
+    example = 'gendiff/tests/fixtures/nested/expect_nested.txt'
+
+    with open(example) as file:
+        data_example = ''.join(line for line in file)
+
+    for file1, file2 in ((file1_json, file2_json), (file1_yaml, file2_yaml)):
+        actual_example = generate_diff(file1, file2)
+        assert data_example == actual_example
