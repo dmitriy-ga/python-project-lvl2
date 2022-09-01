a9b9cbeb357da8fb747d118d6390264d6a330ace
dmitriy-ga <Dmitriy2243@yandex.ru>
2022-09-01 17:40:45 +0500

    Refactoring: output diff dictionary

--- gendiff/gendiff_generator.py
+++ gendiff/gendiff_generator.py
@@ -4,52 +4,40 @@ from gendiff.output_formatters import style_formatting
 
 
 def generate_diff(file1, file2, style='stylish'):
-    def is_dict(item):
-        return isinstance(item, dict)
+    # def is_dict(item):
+    #     return isinstance(item, dict)
 
     def get_diffs(dict1, dict2):
         # Dictionary of untouched items
-        stays = {f'  {key}': value
+        stays = {key: {'action': 'stay', 'value': value}
                  for key, value in dict1.items()
                  if key in dict2
                  and value == dict2.get(key)
                  }
 
         # Dictionary of deleted items
-        deletes = {f'- {key}': value
+        deletes = {key: {'action': 'delete', 'value': value}
                    for key, value in dict1.items()
                    if key not in dict2
-                   or all((key in dict2,
-                           value != dict2.get(key),
-                           not is_dict(value))
-                          )
-                   or is_dict(value) ^ is_dict(dict2.get(key))
                    }
 
         # Dictionary of added items
-        adds = {f'+ {key}': value
+        adds = {key: {'action': 'add', 'value': value}
                 for key, value in dict2.items()
                 if key not in dict1
-                or all((key in dict1,
-                        value != dict1.get(key),
-                        not is_dict(value))
-                       )
-                or is_dict(value) ^ is_dict(dict1.get(key))
                 }
 
-        # Dictionary of changed nested dictionaries
-        modded_dicts = {f'{key}': get_diffs(value, dict2.get(key))
-                        for key, value in dict1.items()
-                        if all((is_dict(value),
-                                is_dict(dict2.get(key)),
-                                key in dict2,
-                                value != dict2.get(key))
-                               )
-                        }
+        # Dictionary of changed items
+        changes = {key: {'action': 'change',
+                         'old_value': value, 'new_value': dict2.get(key)}
+                   for key, value in dict1.items()
+                   if key in dict2
+                   and value != dict2.get(key)
+                   }
 
         # Building and sorting python dictionary
-        result_diffs = OrderedDict(stays | deletes | adds | modded_dicts)
-        for name in sorted(result_diffs, key=lambda x: x.split()[-1]):
+        result_diffs = OrderedDict(stays | deletes | adds | changes)
+        for name in sorted(result_diffs):
             result_diffs.move_to_end(name)
         return result_diffs
 
--- gendiff/output_formatters.py
+++ gendiff/output_formatters.py
@@ -1,8 +1,23 @@
 import json
+from collections import OrderedDict
 
 
 def stylish(item):
-    styled_dict = json.dumps(item, indent=4, separators=('', ': '))
+    formatted_dict = OrderedDict()
+
+    for key, value in item.items():
+        match value['action']:
+            case 'add':
+                formatted_dict[f'+ {key}'] = item[key]['value']
+            case 'delete':
+                formatted_dict[f'- {key}'] = item[key]['value']
+            case 'stay':
+                formatted_dict[f'  {key}'] = item[key]['value']
+            case 'change':
+                formatted_dict[f'- {key}'] = item[key]['old_value']
+                formatted_dict[f'+ {key}'] = item[key]['new_value']
+
+    styled_dict = json.dumps(formatted_dict, indent=4, separators=('', ': '))
     styled_dict = styled_dict.replace('"', '')
     print(styled_dict)
     return styled_dict
