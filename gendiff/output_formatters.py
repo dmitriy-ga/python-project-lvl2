26ddbd23db3dc7dbf253ad2fdb7bae74a4007da7
dmitriy-ga <Dmitriy2243@yandex.ru>
2022-09-06 10:00:27 +0500

    Added styling for nested values

--- gendiff/output_formatters.py
+++ gendiff/output_formatters.py
@@ -3,21 +3,27 @@ from collections import OrderedDict
 
 
 def stylish(item):
-    formatted_dict = OrderedDict()
+    def stylish_format(diff_dict):
+        formatted_dict = OrderedDict()
 
-    for key, value in item.items():
-        match value['action']:
-            case 'add':
-                formatted_dict[f'+ {key}'] = item[key]['value']
-            case 'delete':
-                formatted_dict[f'- {key}'] = item[key]['value']
-            case 'stay':
-                formatted_dict[f'  {key}'] = item[key]['value']
-            case 'change':
-                formatted_dict[f'- {key}'] = item[key]['old_value']
-                formatted_dict[f'+ {key}'] = item[key]['new_value']
+        for key, value in diff_dict.items():
+            match value['action']:
+                case 'add':
+                    formatted_dict[f'+ {key}'] = diff_dict[key]['value']
+                case 'delete':
+                    formatted_dict[f'- {key}'] = diff_dict[key]['value']
+                case 'stay':
+                    formatted_dict[f'  {key}'] = diff_dict[key]['value']
+                case 'change':
+                    formatted_dict[f'- {key}'] = diff_dict[key]['old_value']
+                    formatted_dict[f'+ {key}'] = diff_dict[key]['new_value']
+                case 'nested':
+                    formatted_dict[f'  {key}'] = \
+                        stylish_format(diff_dict[key]['value'])
+        return formatted_dict
 
-    styled_dict = json.dumps(formatted_dict, indent=4, separators=('', ': '))
+    styled_dict = json.dumps(stylish_format(item),
+                             indent=4, separators=('', ': '))
     styled_dict = styled_dict.replace('"', '')
     print(styled_dict)
     return styled_dict
