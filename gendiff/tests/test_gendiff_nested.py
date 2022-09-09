57e95081ecc25d0a51784143c57d0c2d6142dabd
dmitriy-ga <Dmitriy2243@yandex.ru>
2022-09-09 10:38:34 +0500

    Added tests for json style output

--- gendiff/tests/test_gendiff_nested.py
+++ gendiff/tests/test_gendiff_nested.py
@@ -29,3 +29,18 @@ def test_nested_plain():
     for file1, file2 in ((file1_json, file2_json), (file1_yaml, file2_yaml)):
         actual_example = generate_diff(file1, file2, 'plain')
         assert data_example == actual_example
+
+
+def test_nested_json():
+    file1_json = 'gendiff/tests/fixtures/nested/file1.json'
+    file2_json = 'gendiff/tests/fixtures/nested/file2.json'
+    file1_yaml = 'gendiff/tests/fixtures/nested/file1.yaml'
+    file2_yaml = 'gendiff/tests/fixtures/nested/file2.yaml'
+    example = 'gendiff/tests/fixtures/nested/nested_json.txt'
+
+    with open(example) as file:
+        data_example = ''.join(line for line in file)
+
+    for file1, file2 in ((file1_json, file2_json), (file1_yaml, file2_yaml)):
+        actual_example = generate_diff(file1, file2, 'json')
+        assert data_example == actual_example
