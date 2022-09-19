930d2657426c7d1a78b843cc4a5fed9952b56d62
dmitriy-ga <Dmitriy2243@yandex.ru>
2022-09-19 13:24:25 +0500

    Parsing function parted to opening and parsing

--- gendiff/input_reader.py
+++ gendiff/input_reader.py
@@ -3,12 +3,20 @@ import yaml
 
 
 def parsing_file(file):
-    """Detects filename extension and returns
-    converted opened file"""
-    filename_extension = file.split('.')[-1]
+    return parsing_data(*open_file(file))
+
+
+def open_file(file):
+    """Returns inputted file and filename extension of it"""
+    return open(file), file.split('.')[-1]
+
+
+def parsing_data(data, filename_extension):
+    """Converts the input to python dictionary
+    according to the filename extension"""
     match filename_extension:
         case 'json':
-            return json.load(open(file))
+            return json.load(data)
         case 'yaml' | 'yml':
-            yaml_data = yaml.safe_load((open(file)))
+            yaml_data = yaml.safe_load(data)
             return {} if yaml_data is None else yaml_data
